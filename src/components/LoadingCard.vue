b611ccd8c0622278bf109f1d0c5bdb4ff0f4c4b2
memset0 <memset0@outlook.com>
2020-09-24 09:45:38 +0800

    feat: add loading animation

---
+++ src/components/LoadingCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="problem-loading" v-if="show">
+    <mu-card data-mu-loading-color="#B28FCE" v-loading="true"></mu-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoadingCard",
+  props: {
+    show: Boolean,
+  },
+};
+</script>
